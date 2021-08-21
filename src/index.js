699c3c01bfa05962092c7a8a330a9109aeb92a4c
Blackdoll-Loki <helpmeloki7@gmail.com>
2021-08-21 21:21:38 +0300

    init: start morse-decoder task

--- src/index.js
+++ src/index.js
@@ -39,6 +39,28 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let i = 0;
+    let subStrArr = [];
+    let resultStr = "";
+
+    while(i < expr.length){
+    let subStr = expr.substr(i, 10);
+     subStrArr.push(subStr);
+     i += 10;
+    };
+    for (let i = 0; i < subStrArr.length; i++){
+        let spaces = subStrArr[i].replace(/\W+/g, " ");
+        let dashes = spaces.replace(/11/g, "-");
+        let dots = dashes.replace(/10/g, ".");
+        let zeros = dots.replace(/0+/g, "");
+        subStrArr[i] = zeros;
+        if(subStrArr[i] !== " "){
+            resultStr += MORSE_TABLE[subStrArr[i]];
+        } else {
+            resultStr += subStrArr[i];
+        }
+    }
+    return resultStr;
 }
 
 module.exports = {
